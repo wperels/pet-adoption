0e01a4ce8e57fb46b92776fe1a0990aa3e34fd28
Wendy <wperelstein@gmail.com>
2024-03-14 17:04:58 -0600

    Added active styling to current filter button

--- main.js
+++ main.js
@@ -50,4 +50,30 @@ function createAgeText(birthYear) {
   }
 
 
-} 
+}
+
+//pet filter button code
+
+const allButtons = document.querySelectorAll(".pet-filter button")
+
+allButtons.forEach(el => {
+  el.addEventListener("click", handleButtonClick)
+})
+
+function handleButtonClick(e) {
+  // remove active class from any and all buttons
+  allButtons.forEach(el => el.classList.remove("active"))
+
+  // add active class to the specific button that just got clicked
+  e.target.classList.add("active")
+
+  // actually filter the pets down below
+  /*   const currentFilter = e.target.dataset.filter
+    document.querySelectorAll(".pet-card").forEach(el => {
+      if (currentFilter == el.dataset.species || currentFilter == "all") {
+        el.style.display = "grid"
+      } else {
+        el.style.display = "none"
+      }
+    }) */
+}
